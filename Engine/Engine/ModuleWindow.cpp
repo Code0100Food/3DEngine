fd51df2f9fd6bde92cfdfa102b6bc1a5c74c4b8b
Ferran Martin Vilà <ferranmartinvila21@gmail.com>
2017-09-22 11:57:23 +0200

    Window data & config updated

--- Engine/Engine/ModuleWindow.cpp
+++ Engine/Engine/ModuleWindow.cpp
@@ -28,6 +28,10 @@ bool ModuleWindow::Awake(const JSON_Object * data_root)
 	brightness = json_object_get_number(data_root, "brightness");
 	width = json_object_get_number(data_root, "width");
 	height = json_object_get_number(data_root, "height");
+	full_screen = json_object_get_boolean(data_root, "full_screen");
+	resizable = json_object_get_boolean(data_root, "resizable");
+	borderless = json_object_get_boolean(data_root, "borderless");
+	full_desktop = json_object_get_boolean(data_root, "full_desktop");
 
 	return true;
 }
@@ -54,22 +58,22 @@ bool ModuleWindow::Init()
 		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
 		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
 
-		if(WIN_FULLSCREEN == true)
+		if(full_screen == true)
 		{
 			flags |= SDL_WINDOW_FULLSCREEN;
 		}
 
-		if(WIN_RESIZABLE == true)
+		if(resizable == true)
 		{
 			flags |= SDL_WINDOW_RESIZABLE;
 		}
 
-		if(WIN_BORDERLESS == true)
+		if(borderless == true)
 		{
 			flags |= SDL_WINDOW_BORDERLESS;
 		}
 
-		if(WIN_FULLSCREEN_DESKTOP == true)
+		if(full_desktop == true)
 		{
 			flags |= SDL_WINDOW_FULLSCREEN_DESKTOP;
 		}
@@ -114,8 +118,25 @@ void ModuleWindow::BlitConfigInfo()
 {
 	//Brightness slice bar
 	ImGui::SliderFloat("brightness", &brightness, 0.0f, 1.0f);
+	//Width slice bar
 	ImGui::SliderInt("width", &width, 0, SCREEN_WIDTH);
+	//Height slice bar
 	ImGui::SliderInt("height", &height, 0, SCREEN_HEIGHT);
+
+	ImGui::Separator();
+
+	//Full Screen checkbox
+	ImGui::Checkbox("Full Screen", &full_screen);
+	ImGui::SameLine();
+	//Resizable checkbox
+	ImGui::Checkbox("Resizable", &resizable);
+	//Broderless checkbox
+	ImGui::Checkbox("Broderless", &borderless);
+	ImGui::SameLine();
+	//Full Desktop checkbox
+	ImGui::Checkbox("Full Desktop", &full_desktop);
+
+	ImGui::Separator();
 
 	//Apply button
 	if (ImGui::Button("Apply##1", ImVec2(50, 20)))
@@ -123,14 +144,21 @@ void ModuleWindow::BlitConfigInfo()
 		//Apply brightness
 		GLfloat LightModelAmbient[] = { brightness, brightness, brightness, brightness };
 		glLightModelfv(GL_LIGHT_MODEL_AMBIENT, LightModelAmbient);
+		
 		//Save values
 		//Load config json file
 		const JSON_Value *config_data = App->fs->LoadJSONFile("config.json");
 		assert(config_data != NULL);
-		//Save the new variable
+		
+		//Save the new variables
 		json_object_set_number(App->fs->AccessObject(config_data, 1, name.c_str()), "brightness", brightness);
 		json_object_set_number(App->fs->AccessObject(config_data, 1, name.c_str()), "width", width);
 		json_object_set_number(App->fs->AccessObject(config_data, 1, name.c_str()), "height", height);
+		json_object_set_boolean(App->fs->AccessObject(config_data, 1, name.c_str()), "full_screen", full_screen);
+		json_object_set_boolean(App->fs->AccessObject(config_data, 1, name.c_str()), "resizable", resizable);
+		json_object_set_boolean(App->fs->AccessObject(config_data, 1, name.c_str()), "borderless", borderless);
+		json_object_set_boolean(App->fs->AccessObject(config_data, 1, name.c_str()), "full_desktop", full_desktop);
+
 		//Save the file
 		App->fs->SaveJSONFile(config_data, "config.json");
 
