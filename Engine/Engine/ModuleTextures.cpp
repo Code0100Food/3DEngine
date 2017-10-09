bcff5bda7e88f2add2079e6572a6471a25409388
Ferran Martin Vilà <ferranmartinvila21@gmail.com>
2017-10-09 19:30:33 +0200

    Textures load methods improved & fliped textures bug solved

--- Engine/Engine/ModuleTextures.cpp
+++ Engine/Engine/ModuleTextures.cpp
@@ -1,4 +1,5 @@
 #include "ModuleTextures.h"
+#include "Glew/include/glew.h"
 
 #include "Devil/include/il.h"
 #include "Devil/include/ilu.h"
@@ -7,6 +8,7 @@
 #pragma comment (lib, "Engine/Devil/libx86/DevIL.lib")
 #pragma comment (lib, "Engine/Devil/libx86/ILU.lib")
 #pragma comment (lib, "Engine/Devil/libx86/ILUT.lib")
+
 
 // Constructors =================================
 ModuleTextures::ModuleTextures(const char * _name, MODULE_ID _id, bool _config_menu, bool _enabled) :Module(_name, _id, _enabled)
@@ -66,82 +68,74 @@ bool ModuleTextures::Start()
 // Functionality ================================
 uint ModuleTextures::LoadTexture(const char * str)
 {
-	ILuint imageID;				// Create an image ID as a ULuint
+	//Uint where the texture id will be saved
+	ILuint textureID = 0;
+	
+	//Generate gl texture
+	glGenTextures(1, &textureID);
+	
+	//Focus the generated texture
+	glBindTexture(GL_TEXTURE_2D, textureID);
 
-	GLuint textureID;			// Create a texture ID as a GLuint
-
-	ILboolean success;			// Create a flag to keep track of success/failure
-
-	ILenum error;				// Create a flag to keep track of the IL error state
-
-	ilGenImages(1, &imageID); 	// Generate the image ID
-
-	ilBindImage(imageID); 		// Bind the image
-
-	success = ilLoadImage(str); // Load the image file
-
-								// If we managed to load the image, then we can start to do things with it...
+	//Load the image
+	ILboolean success = ilLoadImage(str);
+	
+	//If the image is correctly loaded
 	if (success)
 	{
-		// If the image is flipped (i.e. upside-down and mirrored, flip it the right way up!)
-		ILinfo ImageInfo;
+		/*ILinfo ImageInfo;
 		iluGetImageInfo(&ImageInfo);
 		
-		if (ImageInfo.Origin != IL_ORIGIN_UPPER_LEFT)
+		if (ImageInfo.Origin == IL_ORIGIN_UPPER_LEFT)
 		{
 			iluFlipImage();
-		}
+		}*/
 
-		// Convert the image into a suitable format to work with
-		// NOTE: If your image contains alpha channel you can replace IL_RGB with IL_RGBA
-		//success = ilConvertImage(ilGetInteger(IL_IMAGE_FORMAT), IL_UNSIGNED_BYTE);
+		success = ilConvertImage(ilGetInteger(IL_IMAGE_FORMAT), IL_UNSIGNED_BYTE);
 
 		// Quit out if we failed the conversion
 		if (!success)
 		{
-			error = ilGetError();
-			LOG("[error] Image conversion failed - IL reports error: ", iluErrorString(error));
-			exit(-1);
+			ILenum error = ilGetError();
+			LOG("[error] Loading texture from: %s",str);
+			LOG("[error] %s", iluErrorString(error));
+			ilDeleteImages(1, &textureID);
+			return NULL;
 		}
 
-		glGenTextures(1, &textureID);
-
-		// Bind the texture to a name
-		glBindTexture(GL_TEXTURE_2D, textureID);
-
-
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
-
-		// Set texture interpolation method to use linear interpolation (no MIPMAPS)
+		//Set the texture parameters
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		
+		//Generate the image with the loaded data
+		glTexImage2D(GL_TEXTURE_2D, 		// Type of texture
+			0,								// Pyramid level (for mip-mapping) - 0 is the top level
+			ilGetInteger(IL_IMAGE_FORMAT),	// Internal pixel format to use. Can be a generic type like GL_RGB or GL_RGBA, or a sized type
+			ilGetInteger(IL_IMAGE_WIDTH),	// Image width
+			ilGetInteger(IL_IMAGE_HEIGHT),	// Image height
+			0,								// Border width in pixels (can either be 1 or 0)
+			ilGetInteger(IL_IMAGE_FORMAT),	// Format of image pixel data
+			GL_UNSIGNED_BYTE,				// Image data type
+			ilGetData());					// The actual image data itself
 
-		// Specify the texture specification
-		glTexImage2D(GL_TEXTURE_2D, 			// Type of texture
-			0,									// Pyramid level (for mip-mapping) - 0 is the top level
-			ilGetInteger(IL_IMAGE_FORMAT),		// Internal pixel format to use. Can be a generic type like GL_RGB or GL_RGBA, or a sized type
-			ilGetInteger(IL_IMAGE_WIDTH),		// Image width
-			ilGetInteger(IL_IMAGE_HEIGHT),		// Image height
-			0,									// Border width in pixels (can either be 1 or 0)
-			ilGetInteger(IL_IMAGE_FORMAT),		// Format of image pixel data
-			GL_UNSIGNED_BYTE,					// Image data type
-			ilGetData());						// The actual image data itself
+		glGenerateMipmap(GL_TEXTURE_2D);
 
 
-												// Set texture clamping method
-
+		LOG("Texture: %s correctly loaded!", str);
 	}
-	else // If we failed to open the image file in the first place...
+	else
 	{
-		error = ilGetError();
-		LOG("[error] Image load failed - IL reports error: ", iluErrorString(error));
-		exit(-1);
+		ILenum error = ilGetError();
+		LOG("[error] Loading texture from: %s", str);
+		LOG("[error] %s", iluErrorString(error));
+		ilDeleteImages(1, &textureID);
+		return NULL;
 	}
 	
-	ilDeleteImages(1, &imageID); // Because we have already copied image data into texture data we can release memory used by image.
+	//Delete the image 
+	ilDeleteImages(1, &textureID);
 
-	LOG("Texture creation successful.");
-
-	return textureID; // Return the GLuint to the texture so you can use
+	return textureID;
 }
