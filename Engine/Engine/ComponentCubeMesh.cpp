9c3f3faa3b1375525c7cec1fc133ddeb0104ff58
Ferran Martin Vilà <ferranmartinvila21@gmail.com>
2017-11-02 17:19:45 +0100

    Cube mesh load added!

--- Engine/Engine/ComponentCubeMesh.cpp
+++ Engine/Engine/ComponentCubeMesh.cpp
@@ -2,6 +2,7 @@
 
 #include "ComponentMaterial.h"
 #include "Serializer.h"
+#include "CubeGenerator.h"
 
 // Constructors =================================
 ComponentCubeMesh::ComponentCubeMesh()
@@ -68,6 +69,42 @@ bool ComponentCubeMesh::Save(Serializer & array_root) const
 
 	//Save the built data in the components array
 	ret = array_root.InsertArrayElement(comp_data);
+
+	return ret;
+}
+
+bool ComponentCubeMesh::Load(Serializer & data, std::vector<std::pair<Component*, uint>>& links)
+{
+	bool ret = true;
+
+	//Get component id
+	id = data.GetInt("id");
+	//Get actived
+	actived = data.GetBool("actived");
+
+	//Get geometry data
+	//Get min point
+	Serializer min_point_array = data.GetArray("min_point");
+	for (uint k = 0; k < 3; k++)geometry.minPoint.ptr()[k] = min_point_array.GetArrayFloat(k);
+	//Get max point
+	Serializer max_point_array = data.GetArray("max_point");
+	for (uint k = 0; k < 3; k++)geometry.maxPoint.ptr()[k] = max_point_array.GetArrayFloat(k);
+	//Get divisions
+	divisions = data.GetInt("divisions");
+
+	//Get draw material id
+	uint material_id = data.GetInt("draw_material_id");
+	if (material_id != 0)links.push_back(std::pair<Component*, uint>(this, material_id));
+
+	//Build the mesh with the loaded values
+	std::pair<std::vector<uint>, std::vector<Vertex>> mesh_data;
+	CubeGenerator cube;
+	cube.SetGeometry(geometry);
+	cube.SetDivisions(divisions);
+	mesh_data = cube.Generate();
+	SetIndices(mesh_data.first);
+	SetVertices(mesh_data.second);
+	SetupMesh();
 
 	return ret;
 }
